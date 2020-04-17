e90b1432e5e21c849c585184d27125929cd6ca07
muhuahaha <kimyou2019@gmail.com>
2020-04-17 10:02:57 +0200

    immer noch am testen

--- scripts/main.js
+++ scripts/main.js
@@ -1,86 +1,94 @@
-function sumOfSlices(arr) {}
-console.log(sumOfSlices([58, 3, 38, 99, 10]));
+function blahBlah(str, n) {
+        const arr = str.split(' ');
+        const res =
+                n > arr.length
+                        ? `${`${'blah '.repeat(arr.length - 1)}blah`}...`
+                        : `${arr.map((x, i) => (i >= arr.length - n ? 'blah' : x)).join(' ')}...`;
+        return res;
+}
 
-const testData = `Test.assertSimilar(sumOfSlices([10, 29, 13, 14, 15, 16, 17, 31, 20, 10, 29, 13, 14, 15, 16, 17, 31, 20, 100]), [97, 78, 87, 68, 100])
-Test.assertSimilar(sumOfSlices([58,3,38, 99, 10]), [99, 99, 10])
-Test.assertSimilar(sumOfSlices([13]), [13])
-Test.assertSimilar(sumOfSlices([62, 406, 343, 234, 244, 200, 304, 462, 212, 48, 166, 71, 80, 51, 307, 442, 368, 235, 199, 411, 100, 203, 330, 437, 226, 365, 337, 464, 14, 350]), [62, 406, 343, 234, 244, 200, 304, 462, 212,  48, 166,  71,  80,  51, 307, 442, 368, 235, 199, 411, 100, 203, 330, 437, 226, 365, 337, 464, 14, 350])
-Test.assertSimilar(sumOfSlices([315, 47]), [0, 315, 47])`;
+console.log(blahBlah('A function is a block of code which only runs when it is called', 5));
 
-// console.log(testData);
+const testData = `Test.assertEquals(blahBlah("A function is a block of code which only runs when it is called",  5), "A function is a block of code which only blah blah blah blah blah...");
+Test.assertEquals(blahBlah("one two three four five", 2), "one two three blah blah...");
+Test.assertEquals(blahBlah("Sphinx of black quartz judge my vow", 10), "blah blah blah blah blah blah blah...");
+Test.assertEquals(blahBlah("The quadratic formula is negative b plus or minus the square root of b squared subtract four ac all over two a", 5), "The quadratic formula is negative b plus or minus the square root of b squared subtract four blah blah blah blah blah...");
+`;
 
-// const funk = testData.match(/(?<=\().*(?=,)/g);
-// const ergebnis = testData.match(/(?<=\), ).*(?=\))/g);
+console.log(testData);
+
+const funk = testData.match(/(?<=\().*(?=,)/g);
+const ergebnis = testData.match(/(?<=\), ).*(?=\))/g);
 
 // const ergebnis = testData.match(/(?<=\), ").*(?="\)))/g);  mit String
 
-// function consolo() {
-//         return funk.map((x, i) => `const consolo${i + 1} = ${x}`).join('; <br>');
-// }
+function consolo() {
+        return funk.map((x, i) => `const consolo${i + 1} = ${x}`).join('; <br>');
+}
 
-// function displayErgebnis() {
-//         return ergebnis.map((x, i) => `const ergebnis${i + 1} = ${x}`).join('; <br>');
-// }
+function displayErgebnis() {
+        return ergebnis.map((x, i) => `const ergebnis${i + 1} = ${x}`).join('; <br>');
+}
 
-// // console.log(consolo());
-// // console.log(displayErgebnis());
+// console.log(consolo());
+// console.log(displayErgebnis());
 
-// const resultFunk = consolo();
-// const resultErgebnis = displayErgebnis();
+const resultFunk = consolo();
+const resultErgebnis = displayErgebnis();
 
-// // Make a div
-// const div = document.createElement('div');
-// // add a class of wrapper to it
-// div.classList.add('wrapper');
+// Make a div
+const div = document.createElement('div');
+// add a class of wrapper to it
+div.classList.add('wrapper');
 
-// // put it into the body
-// document.body.appendChild(div);
+// put it into the body
+document.body.appendChild(div);
 
-// // make an unordered list
-// const h2Test = `<h2>
-//   ${funk[0]}
-// </h2><p>${resultFunk} <br><br> ${resultErgebnis}</p>`;
+// make an unordered list
+const h2Test = `<h2>
+  ${funk[0]}
+</h2><p>${resultFunk} <br><br> ${resultErgebnis}</p>`;
 
-// // add three list items with the words "one, two three" in them
-// // put that list into the above wrapper
-// div.innerHTML = h2Test;
+// add three list items with the words "one, two three" in them
+// put that list into the above wrapper
+div.innerHTML = h2Test;
 
-// function generatePlayerCard(funki, ergebnisse, numm, fun) {
-//         const farbeTrue = `${funki.toString()}` === ergebnisse;
+function generatePlayerCard(funki, ergebnisse, numm, fun) {
+        const farbeTrue = `${funki.toString()}` === ergebnisse;
 
-//         const html = `
-//       <div class="playerCard">
-//         <h2>${funki}</h2>
-//         <p>${ergebnisse}<p>
-//         <p>Test ${numm} ist ${farbeTrue}.</p>
-//         <p>${fun}</p>
-//         <div><button class="delete" type="button">&times; Delete</button></div>
-//       </div>
-//     `;
+        const html = `
+      <div class="playerCard">
+        <h2>${funki}</h2>
+        <p>${ergebnisse}<p>
+        <p>Test ${numm} ist ${farbeTrue}.</p>
+        <p>${fun}</p>
+        <div><button class="delete" type="button">&times; Delete</button></div>
+      </div>
+    `;
 
-//         return html;
-// }
+        return html;
+}
 
-// const cards = document.createElement('div');
-// cards.classList.add('cards');
+const cards = document.createElement('div');
+cards.classList.add('cards');
 
-// let cardsHTML = [];
-// for (let i = 0; i < funk.length; i++) {
-//         cardsHTML += generatePlayerCard(eval(funk[i]), ergebnis[i], i + 1, funk[i]);
-// }
+let cardsHTML = [];
+for (let i = 0; i < funk.length; i++) {
+        cardsHTML += generatePlayerCard(eval(funk[i]), ergebnis[i], i + 1, funk[i]);
+}
 
-// cards.innerHTML = cardsHTML;
-// div.insertAdjacentElement('beforebegin', cards);
+cards.innerHTML = cardsHTML;
+div.insertAdjacentElement('beforebegin', cards);
 
-// const buttonsDelete = document.querySelectorAll('.delete');
-// // make out delete function
-// function deleteCard(event) {
-//         const buttonThatGotClicked = event.currentTarget;
-//         // buttonThatGotClicked.parentElement.remove();
-//         buttonThatGotClicked.closest('.playerCard').remove();
-// }
+const buttonsDelete = document.querySelectorAll('.delete');
+// make out delete function
+function deleteCard(event) {
+        const buttonThatGotClicked = event.currentTarget;
+        // buttonThatGotClicked.parentElement.remove();
+        buttonThatGotClicked.closest('.playerCard').remove();
+}
 
-// // loop over them and attach a listener
-// buttonsDelete.forEach(button => button.addEventListener('click', deleteCard));
+// loop over them and attach a listener
+buttonsDelete.forEach(button => button.addEventListener('click', deleteCard));
 
-// // console.log(document.getElementsByClassName('playerCard'));
+// console.log(document.getElementsByClassName('playerCard'));
